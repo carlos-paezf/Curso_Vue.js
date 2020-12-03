53053a0e3f56a74daaaa51bc7335811b64cc5a19
carlos-paezf <carlos.paezf@usantoto.edu.co>
2020-12-02 19:40:03 -0500

    Jugando con Vuetify

--- Parte15-Vuetify_Axios_Vuex/vuetify_axios_vuex/src/main.js
+++ Parte15-Vuetify_Axios_Vuex/vuetify_axios_vuex/src/main.js
@@ -4,11 +4,15 @@ import router from './router'
 import vuetify from './plugins/vuetify';
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  vuetify,
-  render: h => h(App)
+    router,
+    vuetify,
+    render: h => h(App)
 }).$mount('#app')
--- Parte15-Vuetify_Axios_Vuex/vuetify_axios_vuex/src/views/Home.vue
+++ Parte15-Vuetify_Axios_Vuex/vuetify_axios_vuex/src/views/Home.vue
@@ -1,18 +1,160 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <!-- En esta fila aplico una prueba de fila y columna -->
+    <v-layout :wrap="false"> <!-- Se asemeja a la etiqueta <row> -->
+      <v-flex xs12> <!-- Se asemeja a la etiqueta <col> -->
+        <h1>El tamaño xs se hereda para los demas</h1>
+      </v-flex>
+    </v-layout>
+
+    <!-- En esta fila ubico la tarjeta de prueba con Vuetify -->
+    <v-layout>
+      <v-flex xs12>
+        <!-- Caracteristicas Generales de la tarjeta -->
+        <v-card elevation = "24" :loading="loading" class="mx-auto my-5" max-width="500">
+          <template slot="progress">
+            <v-progress-linear color="red" height="10" indeterminate></v-progress-linear>
+          </template>
+          <!-- Imagen en cabecera -->
+          <v-img height="225" src="https://i.morioh.com/2019/11/01/47e709b4198f.jpg">
+            <!-- Seccion para una app bar dentro de la imagen-->
+            <v-app-bar flat color="rgba(0, 0, 0, 0)">
+              <v-app-bar-nav-icon color="white"></v-app-bar-nav-icon>
+              <v-toolbar-title class="title white--text pl-0">
+                App Bar
+              </v-toolbar-title>
+              <v-spacer></v-spacer>
+              <v-btn
+              color="white"
+              icon
+            >
+              <v-icon>add</v-icon>
+            </v-btn>
+            </v-app-bar>
+            <!-- Seccion para un avatar -->
+            <v-card-title class="white--text mt-15">
+              <v-avatar size="56">
+                <img alt="user" src="https://avatars1.githubusercontent.com/u/55883267?s=460&u=050d13a89d2eb274fa862918ce1d2d036d4e037c&v=4">
+              </v-avatar>
+              <p class="ml-3">
+                carlos-paezf
+              </p>
+            </v-card-title>
+          </v-img>
+          <!-- Titulo de la tarjeta -->
+          <v-card-title>Tarjeta con Vuetify</v-card-title>
+          <!-- Definicion del texto -->
+          <v-card-text>
+            <!-- Definir un componente de Rating  -->
+            <v-row align="center" class="mx-2">
+              <v-rating :value="4" color="deep-purple accent-1" dense half-increments readonly size="14"></v-rating>
+              <div class="grey--text ml-4"> 4 (Valor estatico)</div>
+            </v-row>
+            <!-- Definir un subtitulo -->
+            <div class="my-4 subtitle-1">
+              Curso de Vue.js • Vuetify, axios y vuex
+            </div>
+            <div>Pequeña practica para uso de Vuetify obtenuendo datos de una API</div>
+          </v-card-text>
+          <!-- Agregar un espacio divisorio -->
+          <v-divider class="mx-4"></v-divider>
+          <!-- Titulo de la nueva seccion -->
+          <v-card-title>Chips o "Etiquetas" de prueba</v-card-title>
+          <!-- Seccion para texto -->
+          <v-card-text>
+            <!-- Grupo de Chips o Etiquetas -->
+            <v-chip-group v-model="selection" active-class="red white--text" column>
+              <v-btn v-if="!chip1 && !chip2 && !chip3 && !chip4" close color="primary" dark @click="chip1 = true, chip2 = true, chip3 = true, chip4= true">
+                Reset Chips
+              </v-btn>
+              <v-chip v-if="chip1" class="ma-2" close @click:close="chip1 = false">
+                Closable
+              </v-chip>
+              <v-chip v-if="chip2" class="ma-2" close color="red" text-color="white" @click:close="chip2 = false">
+                Remove
+              </v-chip>
+              <v-chip v-if="chip3" class="ma-2" close color="green" outlined @click:close="chip3 = false">
+                Success
+              </v-chip>
+              <v-chip v-if="chip4" class="ma-2" close color="orange" label outlined @click:close="chip4 = false">
+                Complete
+              </v-chip>
+            </v-chip-group>
+          </v-card-text>
+          <!-- Boton clasico -->
+          <v-card-actions>
+            <v-btn color="deep-purple lighten-2" text @click="reserve">
+              Hacer que aparezca el loading
+            </v-btn>
+          </v-card-actions>
+          <!-- Agregar un espacio divisorio -->
+          <v-divider class="mx-4"></v-divider>
+          <!-- Componente de Timeline -->
+          <v-card-text>
+            <div class="font-weight-bold ml-8 mb-2">Timeline con mensajes de prueba</div>
+            <v-timeline align-top dense>
+              <!-- Los items se obtienen por medio de un bucle que afecta los mensajes -->
+              <v-timeline-item v-for="message in messages" :key="message.time" :color="message.color" small>
+                <div>
+                  <div class="font-weight-normal">
+                    <strong>{{ message.from }}</strong> @{{ message.time }}
+                  </div>
+                  <div>{{ message.message }}</div>
+                </div>
+              </v-timeline-item>
+            </v-timeline>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  data: () => ({
+      loading: false,
+      selection: 1,
+      chip1: true,
+      chip2: true,
+      chip3: true,
+      chip4: true,
+      messages: [
+        {
+          from: 'You esperanzado',
+          message: 'Escribir "Hola Mundo"',
+          time: 'Paso 1',
+          color: 'green',
+        },
+        {
+          from: 'You feliz',
+          message: 'Pensar que ya eres programador',
+          time: 'Paso 2',
+          color: 'blue',
+        },
+        {
+          from: 'You triste',
+          message: 'Ver que aun te falta mucho por aprender',
+          time: 'Paso 3',
+          color: 'red',
+        },
+        {
+          from: 'You con ganas',
+          message: 'Tomar fuerzas y seguir aprendiendo',
+          time: 'Paso 4',
+          color: 'deep-purple',
+        }
+      ],
+    }),
+
+    methods: {
+      reserve () {
+        this.loading = true
+        setTimeout(() => (this.loading = false), 5000)
+      },
+    },
 }
 </script>
