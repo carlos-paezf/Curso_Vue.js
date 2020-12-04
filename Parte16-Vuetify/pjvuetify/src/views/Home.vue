ed71872a99a80782771734178bd35bb20b8431e2
carlos-paezf <carlos.paezf@usantoto.edu.co>
2020-12-03 20:09:10 -0500

    Jugando con el texto

---
+++ Parte16-Vuetify/pjvuetify/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <v-layout wrap class="deep-orange accent-4" style="height:150px" align-center justify-center>
+      <v-flex xs7 class="white text-center">
+        <h1>Centrar la columna de manera vertical y horinzontal</h1>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+}
+</script>
