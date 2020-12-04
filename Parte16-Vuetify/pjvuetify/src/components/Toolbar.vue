0c11d2c502ba0a5a09e407eeb6001eb3959485f0
carlos-paezf <carlos.paezf@usantoto.edu.co>
2020-12-04 16:43:54 -0500

    Botones, Iconos y Toolbar

---
+++ Parte16-Vuetify/pjvuetify/src/components/Toolbar.vue
@@ -0,0 +1,95 @@
+<template>
+    <div>
+        <!-- Toolbar fijo en la pagina con iconos -->
+        <v-toolbar app dark>
+            <!-- Cada vez que presiono el icono, aparece el menu lateral -->
+            <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+            <v-toolbar-title>Proyecto de Prueba</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <!-- Seccion para botones que sirvan de enlace -->
+            <router-link to="/">
+                <v-btn text>Home</v-btn>
+            </router-link>
+            <router-link to="/botones">
+                <v-btn text>Botones</v-btn> 
+            </router-link>
+            
+            <v-spacer></v-spacer>
+            <v-btn icon>
+                <v-icon>mdi-magnify</v-icon>
+            </v-btn>
+            <v-btn icon>
+                <v-icon>mdi-export</v-icon>
+            </v-btn>
+        </v-toolbar>
+
+        <!-- Menu lateral con items varios -->
+        <v-navigation-drawer app v-model="drawer" temporary dark>
+            <v-list nav dense>
+                <v-list-item class="px-2">
+
+                    <!-- Solucion para centrar el avatar -->
+                    <v-layout column align-center>
+                        <v-flex>
+                            <v-list-item-avatar>
+                                <v-img src="https://avatars1.githubusercontent.com/u/55883267?s=460&u=050d13a89d2eb274fa862918ce1d2d036d4e037c&v=4"></v-img>
+                            </v-list-item-avatar>
+                        </v-flex>
+                    </v-layout>
+                </v-list-item>
+
+                <!-- Seccion para enlaces de info -->
+                <v-list-item link>
+                    <v-list-item-content>
+                        <v-list-item-title class="title my-3">
+                            carlos-paezf
+                        </v-list-item-title>
+                        <v-list-item-subtitle>https://github.com/carlos-paezf/Curso_Vue.js</v-list-item-subtitle>
+                        <v-btn class="my-3">Editar Perfil</v-btn>
+                    </v-list-item-content>
+                </v-list-item>
+
+                <!-- Seccion divisoria entre las diferentes secciones -->
+                <v-divider class="mb-10 mt-5"></v-divider>
+
+                <!-- Elementos que se encuentran dentro del menu lateral -->
+                <v-list-item-group v-model="group" active-class="white--text text--accent-4">
+                    <!-- Estos items tambien se puden extraer con un bucle desde el data de script -->
+                    <v-list-item>
+                        <v-list-item-icon>
+                            <v-icon>mdi-folder</v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-title>Mis Archivos</v-list-item-title>
+                    </v-list-item>
+                    <v-list-item>
+                        <v-list-item-icon>
+                            <v-icon>mdi-account-multiple</v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-title>Amigos</v-list-item-title>
+                    </v-list-item>
+                    <v-list-item>
+                        <v-list-item-icon>
+                            <v-icon>mdi-star</v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-title>Favoritos</v-list-item-title>
+                    </v-list-item>
+                </v-list-item-group>
+            </v-list>
+        </v-navigation-drawer>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Toolbar',
+    data: () => ({
+        drawer: true,
+        group: null
+    }),
+    watch: {
+        group () {
+            this.drawer = false
+        },
+    },
+}
+</script>
---
+++ Parte16-Vuetify/pjvuetify/src/views/Botones.vue
@@ -0,0 +1,90 @@
+<template>
+  <v-container>
+    <v-layout>
+      <v-flex>
+        <!-- Personalizar el color del boton -->
+        <v-btn dark color="orange acent-4">Boton en color personalizado</v-btn>
+        <!-- Boton de solo texto -->
+        <v-btn class="mx-2" color="secondary" text>Boton sin fondo</v-btn>
+        <!-- Solo la linea externa del boton -->
+        <v-btn class="ma-2" outlined color="indigo">Boton outlined</v-btn>
+        <!-- Boton redondo de solo icono -->
+        <v-btn class="mx-2" fab dark color="purple">
+          <v-icon dark>mdi-pencil</v-icon>
+        </v-btn>
+        <!-- Boton de solo icono -->
+        <v-btn class="ma-2" icon color="deep-orange" >
+          <v-icon>mdi-thumb-up</v-icon>
+        </v-btn>
+        <!-- Boton con carga -->
+        <v-btn class="ma-2" :loading="loading4" :disabled="loading4" color="info" @click="loader = 'loading4'">
+          Icon Loader
+          <template v-slot:loader>
+            <span class="custom-loader">
+              <v-icon light>cached</v-icon>
+            </span>
+          </template>
+        </v-btn>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Botones',
+  data () {
+    return {
+      loader: null,
+      loading4: false,
+    }
+  },
+  watch: {
+    loader () {
+      const l = this.loader
+      this[l] = !this[l]
+      setTimeout(() => (this[l] = false), 3000)
+      this.loader = null
+    },
+  },
+}
+</script>
+
+<style>
+  .custom-loader {
+    animation: loader 1s infinite;
+    display: flex;
+  }
+  @-moz-keyframes loader {
+    from {
+      transform: rotate(0);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+  @-webkit-keyframes loader {
+    from {
+      transform: rotate(0);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+  @-o-keyframes loader {
+    from {
+      transform: rotate(0);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+  @keyframes loader {
+    from {
+      transform: rotate(0);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+</style>
