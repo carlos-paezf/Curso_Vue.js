91a7889e6a089d1aa904378bf42247b9e9e45aed
carlos-paezf <carlos.paezf@usantoto.edu.co>
2020-12-11 17:13:01 -0500

    Añadiendo NotasController

--- CRUD-Laravel-VueJs/routes/web.php
+++ CRUD-Laravel-VueJs/routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::resource('/notas', 'NotasController')->middleware('auth');
+
+Route::get('/home' , 'HomeController@index')->name('home');
